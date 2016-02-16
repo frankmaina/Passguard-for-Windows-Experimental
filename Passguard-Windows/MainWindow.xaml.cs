9a2f3e9cffceefbadc8ac55cfe49eddd4a9d99cd
Francis Maina <frankmaina@live.com>
2016-02-16 10:08:29 +0300

    Initial commit
    UI Inititalisation only.

---
+++ Passguard-Windows/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Passguard_Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ImagePanel_Drop(object sender, DragEventArgs e)
+        {
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // the user can drag in more than one file so wqe only need to use the first selected
+                //file
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                //array files holds the paths to the files
+            }
+        }
+    }
+}
