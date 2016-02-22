8d4ebf9524aea595b3c4e3419c0489772bfb0758
francis maina <frankmaina@live.com>
2016-02-22 21:07:36 +0300

    More user experience enhancements and crash fixing

--- Passguard-Windows/MainWindow.xaml.cs
+++ Passguard-Windows/MainWindow.xaml.cs
@@ -20,7 +20,8 @@ namespace Passguard_Windows
     /// </summary>
     public partial class MainWindow : Window
     {
-        String file;
+        public static String file;
+
 
         public MainWindow()
         {
@@ -29,6 +30,7 @@ namespace Passguard_Windows
 
         private void ImagePanel_Drop(object sender, DragEventArgs e)
         {
+        
 
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
@@ -37,28 +39,48 @@ namespace Passguard_Windows
                 string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
                 //array files holds the paths to the files
                 //first declare file types that are not allowed
-                string[] unallowed_files = {".exe",".lnk",".bat"};
+                string[] unallowed_files = {
+                                            ".exe",
+                                            ".lnk",
+                                            ".bat",
+                                            ".ini"
+                                        };
 
                 //we then check for the file extension
-                foreach ( string unallowed_file in unallowed_files){
+                foreach (string unallowed_file in unallowed_files)
+                {
                     string extension = Path.GetExtension(files[0]);
                     if (extension == unallowed_file)
                     {
                         //file type is forbidden
-                        MessageBox.Show("File type is forbidden");
-                       
+                        MessageBox.Show("File type "+unallowed_file+" is forbidden.");
+                        files[0] = null;
                     }
 
                 }
+                if (files[0]!= null){
+
+                //update UI showing file has been recieved
+                noticeString.Content = "File: "+Path.GetFileName(files[0]) + " has been received.";
+                //close uploading
+                ImagePanel.IsEnabled = false;
+                noticeString.FontSize = 15;
                 //we now read the file to base64 encoding
                 Byte[] bytes = File.ReadAllBytes(files[0]);
                 file = Convert.ToBase64String(bytes);
-                
-               
 
+                }
             }
         }
 
-       
+        private void encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            if (file == null)
+            {
+                MessageBox.Show("Nothing to encrypt");
+            }
+        }
+
+
     }
 }
