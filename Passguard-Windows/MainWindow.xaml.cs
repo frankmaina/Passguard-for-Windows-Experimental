cefc8f51f43049657f150fa2f0494cd5cd7956f5
francis maina <frankmaina@live.com>
2016-02-22 20:09:30 +0300

    Base64 encoding of file

--- Passguard-Windows/MainWindow.xaml.cs
+++ Passguard-Windows/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
 
 namespace Passguard_Windows
 {
@@ -34,6 +34,26 @@ namespace Passguard_Windows
                 //file
                 string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
                 //array files holds the paths to the files
+                //first declare file types that are not allowed
+                string[] unallowed_files = {".exe",".lnk",".bat"};
+
+                //we then check for the file extension
+                foreach ( string unallowed_file in unallowed_files){
+                    string extension = Path.GetExtension(files[0]);
+                    if (extension == unallowed_file)
+                    {
+                        //file type is forbidden
+                        MessageBox.Show("File type is forbidden");
+                       
+                    }
+
+                }
+                //we now read the file to base64 encoding
+                Byte[] bytes = File.ReadAllBytes(files[0]);
+                String file = Convert.ToBase64String(bytes);
+                
+                MessageBox.Show(file.ToString());
+
             }
         }
     }
